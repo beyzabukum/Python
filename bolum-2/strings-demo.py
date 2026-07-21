4d55230373dfe89a752ff9ea97058f9923818b85
Beyza Bukum <beyzabukum@icloud.com>
2026-07-21 14:43:11 +0300

    python egitim

---
+++ bolum-2/strings-demo.py
@@ -0,0 +1,23 @@
+website = "http://www.sadikturan.com"
+course = "Python Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
+"""
+print(len(course))
+print(website[7:10])
+print(website[22:25])
+print(course[:15])
+print(course[-15:])
+print(course[::-1])
+
+"""
+
+name, surname, age, job = "Bora", "Yılmaz", 32, "mühendis"
+print(f"Benim adım {name} {surname}, Yaşım {age} ve mesleğim {job}.")
+
+s="Hello world"
+result=s[0:6]+ "W" +s[-4:]
+s.replace("W","w")
+print(s)
+
+x="abc"
+result=3*x
+print(result)
---
+++ bolum-2/variables-demo.py
@@ -0,0 +1,22 @@
+customer_name="Beyza"
+customer_surname="Büküm"
+customer_nameSurname=customer_name +""+ customer_surname
+customer_gender=True #Women
+customer_id="11111111110"
+customer_birthday_year=2003
+customer_address="abc"
+customer_age=2026 - customer_birthday_year
+print(customer_age)
+
+
+"""
+
+
+"""
+order1=110
+order2=1100.5
+order3=356.95
+
+total=order1+order2+order3
+
+print("Total :",total)
