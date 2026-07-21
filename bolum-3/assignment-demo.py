52b42ebc3b55111250f466a2d02035e5f1e4e76e
Beyza Bukum <beyzabukum@icloud.com>
2026-07-21 21:34:02 +0300

    python egitim

---
+++ bolum-3/assignment-demo.py
@@ -0,0 +1,24 @@
+x,y,z=2,5,107
+numbers=1,5,7,10,6
+"""
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+result=a*b
+toplam=x+y+z
+print(result-toplam)
+
+"""
+"""
+result=(y//x)
+print(result)
+
+"""
+"""
+toplam=x+y+z
+result=toplam%3
+print(result)
+
+"""
+
+result=y**x
+print(result)
---
+++ bolum-3/comparison-demo.py
@@ -0,0 +1,12 @@
+"""
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+result=(a>b)
+print(f"a:{a} b:{b} den büyüktür:{result}")
+
+"""
+vize1=float(input("1.vize:"))
+vize2=float(input("2.vize:"))
+final=float(input("final:"))
+ortalama=((vize1+vize2))/2 *0.6 +(final*0.4)
+print(f"not ortalamanız{ortalama} ve dersten geçme durunuz {ortalama>=50}")
