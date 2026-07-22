3c779ad99ee715a7f98cb1f6ce64ffc5b391c2e4
Beyza Bukum <beyzabukum@icloud.com>
2026-07-22 20:31:45 +0300

    python egitim

---
+++ bolum6/scope.py
@@ -0,0 +1,36 @@
+x="global x"
+def function():
+    x="local x"
+    print(x)
+
+function()
+print(x)
+
+#############################
+
+name="çınar"
+def changeName(new_name):
+    name=new_name
+    print(name)
+changeName("ada")
+print(name)
+
+
+#######################
+
+name="global string"
+def greeting():
+    name="çınar"
+    def hello():
+        print("hello"+ name)
+    hello()
+greeting()            
+
+########################
+
+x=50
+def test(x):
+    print(f"x: {x}")
+    x=100
+    print(f"changed x to{x}")
+test(x)
